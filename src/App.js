22686da01be039976e246ac7d086c8303a442158
Rgonzales4 <roshel.gonzales@yahoo.com>
2022-06-05 13:39:23 +1000

    feat: navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 function App() {
   return (
     <>
-      <h1> Hello World!</h1>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
