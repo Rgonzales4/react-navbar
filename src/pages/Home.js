c7cdcc797f7c4ba50691407d92885fa889fef129
Rgonzales4 <roshel.gonzales@yahoo.com>
2022-08-02 21:02:47 +1000

    feat: :sparkles: trying the google map API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 function Home() {
-  return <h1>Home</h1>;
+  const [enableMap, setEnableMap] = React.useState(false);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: null, //Insert key here
+  });
+
+  if (!isLoaded) {
+    return <div>Map Loaded</div>;
+  }
+
+  return (
+    <>
+      <h1>Home</h1>
+      {enableMap && (
+        <GoogleMap
+          zoom={10}
+          center={{ lat: -33.766098, lng: 150.912693 }}
+          mapContainerClassName="mapContainer"
+        />
+      )}
+    </>
+  );
 }
 
 export default Home;
